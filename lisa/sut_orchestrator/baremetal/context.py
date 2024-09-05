21e2c09c3f3643a5ae16857e9a6bb9103398d9c2
smit-gardhariya <gardhariya.smit@gmail.com>
2024-09-05 10:09:16 +0530

    Revert "Use default_factory to initialize ConnectionInfo"
    This reverts commit f1bb82e03a37f888cc92e3a3af6ab528c996c1ea.

--- lisa/sut_orchestrator/baremetal/context.py
+++ lisa/sut_orchestrator/baremetal/context.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 from lisa import schema
 from lisa.environment import Environment
@@ -14,7 +14,7 @@ class EnvironmentContext:
 
 @dataclass
 class NodeContext:
-    connection: schema.ConnectionInfo = field(default_factory=schema.ConnectionInfo)
+    connection: schema.ConnectionInfo = schema.ConnectionInfo(password="mock")
 
 
 @dataclass
