634078fde1f7bd571fda9630799b98dffc892027
Sonia Sharma <sosha@microsoft.com>
2024-09-18 14:01:51 -0700

    Add provisioning test case for premiumV2 disk

--- microsoft/testsuites/core/provisioning.py
+++ microsoft/testsuites/core/provisioning.py
@@ -152,6 +152,28 @@ class Provisioning(TestSuite):
 
     @TestCaseMetadata(
         description="""
+        This case runs smoke test on a node provisioned with premium disk.
+        The test steps are same as `smoke_test`.
+        """,
+        priority=1,
+        requirement=simple_requirement(
+             disk=schema.DiskOptionSettings(
+                data_disk_type=schema.DiskType.PremiumV2SSDLRS,
+                data_disk_count=search_space.IntRange(min=1),
+            ),
+            environment_status=EnvironmentStatus.Deployed,
+            supported_features=[SerialConsole],
+        ),
+    )
+    def verify_deployment_provision_premiumv2_disk(
+        self, log: Logger, node: RemoteNode, log_path: Path
+    ) -> None:
+        self._smoke_test(
+            log, node, log_path, "verify_deployment_provision_premiumv2_disk"
+        )
+
+    @TestCaseMetadata(
+        description="""
         This case runs smoke test on a node provisioned with sriov.
         The test steps are same as `smoke_test`.
         """,
